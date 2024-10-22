bf92285f0c6827643c9163731c2e5bab606e5fd4
Timir Bhingradiya <95134530+TTimir@users.noreply.github.com>
2024-10-22 22:24:48 +0530

    Add project files.

---
+++ EX_IdentityApp/Areas/Identity/Data/IdentityAppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace EX_IdentityApp.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the IdentityAppUser class
+public class IdentityAppUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
